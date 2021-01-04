34424973507422b05cd6f1bf0cd51d874a46d182
William Schwartz <wkschwartz@gmail.com>
2021-01-04 12:04:28 -0600

    Correct or remove misleading code comments
    The comment in `questioner.py` about needing `getattr()` for `__file__` only in
    PY36 and older is true only if you
    * only use CPython
    * are not talking about `__main__` (which, to be fair, applies in context)
    * never use frozen modules

--- django/utils/module_loading.py
+++ django/utils/module_loading.py
@@ -86,7 +86,7 @@ def module_dir(module):
     Raise ValueError otherwise, e.g. for namespace packages that are split
     over several directories.
     """
-    # Convert to list because _NamespacePath does not support indexing.
+    # Convert to list because __path__ is only required to be Iterable[str]
     paths = list(getattr(module, '__path__', []))
     if len(paths) == 1:
         return paths[0]
